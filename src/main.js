c47104be7c4b566e9f4150e30b51911992fe43a6
Matthias Kuhs <matthiku@yahoo.com>
2018-01-10 16:18:23 +0000

    before refactoring into single file components

--- src/main.js
+++ src/main.js
@@ -21,17 +21,31 @@ new Vue({
   },
   components: {
     'movie-list': {
+      props: ['genre', 'time'],
       template: `
         <div id="movie-list">
-          <div v-for="movie in movies" class="movie">{{ movie.title }}</div>
+          <div v-for="movie in filteredMovies" class="movie">{{ movie.title }}</div>
         </div>`,
       data: () => {
         return {
           movies: [
-            { title: 'Pulp Fiction' },
-            { title: 'Home Alone' },
-            { title: 'Austin Powers' }
+            { title: 'Pulp Fiction', genre: genres.CRIME },
+            { title: 'Home Alone', genre: genres.COMEDY },
+            { title: 'Austin Powers', genre: genres.COMEDY }
           ]
+        }
+      },
+      methods: {
+        moviePassesGenreFilter (movie) {
+          // show all if the filter is empty
+          if (!this.genre.length) { return true }
+          // check if the genre of the movie is in the list of filters
+          return this.genre.find(genre => movie.genre === genre)
+        }
+      },
+      computed: {
+        filteredMovies () {
+          return this.movies.filter(this.moviePassesGenreFilter)
         }
       }
     },
